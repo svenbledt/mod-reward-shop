e4f121f4c5b16c5af44c8acd05c73daf07cab35a
Helias <stefanoborzi32@gmail.com>
2022-03-27 13:50:47 +0200

    fix: build

--- src/reward_shop.cpp
+++ src/reward_shop.cpp
@@ -26,25 +26,25 @@ script made by talamortis
 class reward_shop : public CreatureScript
 {
 public:
-    reward_shop()   :   CreatureScript("reward_shop"){  }
+    reward_shop() : CreatureScript("reward_shop") {}
 
     bool failedcode;
 
-    bool OnGossipHello(Player* player, Creature* creature)
+    bool OnGossipHello(Player *player, Creature *creature)
     {
         if (player->IsInCombat())
             return false;
 
-        if (!sConfigMgr->GetBoolDefault("RewardShopEnable", 0))
+        if (!sConfigMgr->GetOption<bool>("RewardShopEnable", 0))
             return false;
-
 
         std::string text = "Enter code and press accept";
         AddGossipItemFor(player, GOSSIP_ICON_CHAT, "I'd like to redeem my code.", GOSSIP_SENDER_MAIN, 1, text, 0, true);
         AddGossipItemFor(player, GOSSIP_ICON_CHAT, "How do i get a code?", GOSSIP_SENDER_MAIN, 2);
         AddGossipItemFor(player, GOSSIP_ICON_CHAT, "I dont have a code.", GOSSIP_SENDER_MAIN, 3);
 
-        if (sConfigMgr->GetBoolDefault("AllowGM", 1) && player->IsGameMaster()) {
+        if (sConfigMgr->GetOption<bool>("AllowGM", 1) && player->IsGameMaster())
+        {
             AddGossipItemFor(player, GOSSIP_ICON_BATTLE, "[GM] I would like generate a code.", GOSSIP_SENDER_MAIN, 4);
         }
 
@@ -52,11 +52,10 @@ public:
         return true;
     }
 
-
-    bool OnGossipSelect(Player* player, Creature* creature, uint32 /* sender */, uint32 action)
+    bool OnGossipSelect(Player *player, Creature *creature, uint32 /* sender */, uint32 action)
     {
         player->PlayerTalkClass->ClearMenus();
-        std::string info = sConfigMgr->GetStringDefault("WebsiteAddress", "You can get codes by visiting the online store at (website address)");
+        std::string info = sConfigMgr->GetOption<std::string>("WebsiteAddress", "You can get codes by visiting the online store at (website address)");
         uint32 rnd1 = urand(10000, 90000);
         uint32 rnd2 = urand(10000, 90000);
         uint32 rnd3 = urand(10000, 90000);
@@ -84,36 +83,36 @@ public:
             SendGossipMenuFor(player, DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
             break;
         case 6:
-            CharacterDatabase.PQuery("INSERT INTO `reward_shop` (`action`, `action_data`, `quantity`, `code`, `status`, `PlayerGUID`, `PlayerIP`, `CreatedBy`) VALUES(3, 0, 0, '%s', 0, 0, '0', '%s')", randomcode.str().c_str(), CreatedBy.c_str());
+            CharacterDatabase.Query("INSERT INTO `reward_shop` (`action`, `action_data`, `quantity`, `code`, `status`, `PlayerGUID`, `PlayerIP`, `CreatedBy`) VALUES(3, 0, 0, '%s', 0, 0, '0', '%s')", randomcode.str().c_str(), CreatedBy.c_str());
             ChatHandler(player->GetSession()).PSendSysMessage("Code was successfully created your code is %s", randomcode.str().c_str());
             break;
         case 7:
-            CharacterDatabase.PQuery("INSERT INTO `reward_shop` (`action`, `action_data`, `quantity`, `code`, `status`, `PlayerGUID`, `PlayerIP`,`CreatedBy`) VALUES(4, 0, 0, '%s', 0, 0, '0', '%s')", randomcode.str().c_str(), CreatedBy.c_str());
+            CharacterDatabase.Query("INSERT INTO `reward_shop` (`action`, `action_data`, `quantity`, `code`, `status`, `PlayerGUID`, `PlayerIP`,`CreatedBy`) VALUES(4, 0, 0, '%s', 0, 0, '0', '%s')", randomcode.str().c_str(), CreatedBy.c_str());
             ChatHandler(player->GetSession()).PSendSysMessage("Code was successfully created your code is %s", randomcode.str().c_str());
             break;
         case 8:
-            CharacterDatabase.PQuery("INSERT INTO `reward_shop` (`action`, `action_data`, `quantity`, `code`, `status`, `PlayerGUID`, `PlayerIP`, `CreatedBy`) VALUES(5, 0, 0, '%s', 0, 0, '0', '%s')", randomcode.str().c_str(), CreatedBy.c_str());
+            CharacterDatabase.Query("INSERT INTO `reward_shop` (`action`, `action_data`, `quantity`, `code`, `status`, `PlayerGUID`, `PlayerIP`, `CreatedBy`) VALUES(5, 0, 0, '%s', 0, 0, '0', '%s')", randomcode.str().c_str(), CreatedBy.c_str());
             ChatHandler(player->GetSession()).PSendSysMessage("Code was successfully created your code is %s", randomcode.str().c_str());
             break;
         }
         return true;
     }
 
-    bool OnGossipSelectCode(Player* player, Creature* creature, uint32 /* sender */, uint32, const char* code)
+    bool OnGossipSelectCode(Player *player, Creature *creature, uint32 /* sender */, uint32, const char *code)
     {
         ObjectGuid playerguid = player->GetGUID();
         std::string playerIP = player->GetSession()->GetRemoteAddress();
         std::string rewardcode = code;
         std::ostringstream messageCode;
         messageCode << "Sorry " << player->GetName() << ", that is not a valid code or has already been redeemed.";
-        
+
         std::size_t found = rewardcode.find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-");
 
         if (found != std::string::npos)
             return false;
 
-        //check for code
-        QueryResult result = CharacterDatabase.PQuery("SELECT id, action, action_data, quantity, status FROM reward_shop WHERE code = '%s'", rewardcode.c_str());
+        // check for code
+        QueryResult result = CharacterDatabase.Query("SELECT id, action, action_data, quantity, status FROM reward_shop WHERE code = '%s'", rewardcode.c_str());
 
         if (!result)
         {
@@ -128,72 +127,70 @@ public:
 
         do
         {
-                Field* fields = result->Fetch();
-                uint32 action = fields[1].GetUInt32();
-                uint32 action_data = fields[2].GetUInt32();
-                uint32 quantity = fields[3].GetUInt32();
-                uint32 status = fields[4].GetInt32();
-                int count = 1;
-                uint32 noSpaceForCount = 0;
-                ItemPosCountVec dest;
-                InventoryResult msg = player->CanStoreNewItem(NULL_BAG, NULL_SLOT, dest, action_data, quantity, &noSpaceForCount);
+            Field *fields = result->Fetch();
+            uint32 action = fields[1].Get<uint32>();
+            uint32 action_data = fields[2].Get<uint32>();
+            uint32 quantity = fields[3].Get<uint32>();
+            uint32 status = fields[4].Get<int32>();
+            int count = 1;
+            uint32 noSpaceForCount = 0;
+            ItemPosCountVec dest;
+            InventoryResult msg = player->CanStoreNewItem(NULL_BAG, NULL_SLOT, dest, action_data, quantity, &noSpaceForCount);
 
+            if (status == 1)
+            {
+                player->PlayDirectSound(9638); // No
+                creature->Whisper(messageCode.str().c_str(), LANG_UNIVERSAL, player);
+                creature->HandleEmoteCommand(EMOTE_ONESHOT_QUESTION);
+                SendGossipMenuFor(player, DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
+                return false;
+            }
+            switch (action)
+            {
 
-                if (status == 1)
+            case 1: /* Item */
+                if (msg != EQUIP_ERR_OK)
+                    count -= noSpaceForCount;
+
+                if (count == 0 || dest.empty())
                 {
-                    player->PlayDirectSound(9638); // No
-                    creature->Whisper(messageCode.str().c_str(), LANG_UNIVERSAL, player);
-                    creature->HandleEmoteCommand(EMOTE_ONESHOT_QUESTION);
-                    SendGossipMenuFor(player, DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
+                    ChatHandler(player->GetSession()).PSendSysMessage("Can not create item either item is unique or you do not have any space");
+                    ChatHandler(player->GetSession()).SetSentErrorMessage(true);
                     return false;
                 }
-                switch (action)
+
+                if (count > 0 && action_data)
                 {
-
-                case 1: /* Item */
-                    if (msg != EQUIP_ERR_OK)
-                        count -= noSpaceForCount;
-
-                    if (count == 0 || dest.empty())
-                    {
-                        ChatHandler(player->GetSession()).PSendSysMessage("Can not create item either item is unique or you do not have any space");
-                        ChatHandler(player->GetSession()).SetSentErrorMessage(true);
-                        return false;
-                    }
-
-                    if (count > 0 && action_data)
-                    {
-                        player->AddItem(action_data, quantity);
-                    }
-                    break;
-                case 2: /* Gold */
-                    player->ModifyMoney(action_data * 10000);
-                    ChatHandler(player->GetSession()).PSendSysMessage("CHAT OUTPUT: Successfully added [%u G]", action_data);
-                    break;
-                case 3: /* Name Change */
-                    player->SetAtLoginFlag(AT_LOGIN_RENAME);
-                    ChatHandler(player->GetSession()).PSendSysMessage("CHAT OUTPUT: Please log out for name change.");
-                    break;
-                case 4: /* Faction Change */
-                    player->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
-                    ChatHandler(player->GetSession()).PSendSysMessage("CHAT OUTPUT: Please log out for faction change.");
-                    break;
-                case 5: /* Race Change */
-                    player->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
-                    ChatHandler(player->GetSession()).PSendSysMessage("CHAT OUTPUT: Please log out for race change.");
-                    break;
+                    player->AddItem(action_data, quantity);
                 }
+                break;
+            case 2: /* Gold */
+                player->ModifyMoney(action_data * 10000);
+                ChatHandler(player->GetSession()).PSendSysMessage("CHAT OUTPUT: Successfully added [%u G]", action_data);
+                break;
+            case 3: /* Name Change */
+                player->SetAtLoginFlag(AT_LOGIN_RENAME);
+                ChatHandler(player->GetSession()).PSendSysMessage("CHAT OUTPUT: Please log out for name change.");
+                break;
+            case 4: /* Faction Change */
+                player->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
+                ChatHandler(player->GetSession()).PSendSysMessage("CHAT OUTPUT: Please log out for faction change.");
+                break;
+            case 5: /* Race Change */
+                player->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
+                ChatHandler(player->GetSession()).PSendSysMessage("CHAT OUTPUT: Please log out for race change.");
+                break;
+            }
 
         } while (result->NextRow());
 
-        CharacterDatabase.PQuery("UPDATE reward_shop SET status = 1, PlayerGUID = '%u', PlayerIP = '%s' WHERE code = '%s'", playerguid.GetCounter(), playerIP.c_str(), rewardcode.c_str());
+        CharacterDatabase.Query("UPDATE reward_shop SET status = 1, PlayerGUID = '%u', PlayerIP = '%s' WHERE code = '%s'", playerguid.GetCounter(), playerIP.c_str(), rewardcode.c_str());
         return true;
     }
 
-
-    struct npc_reward_shopAI: public ScriptedAI
+    struct npc_reward_shopAI : public ScriptedAI
     {
-        npc_reward_shopAI(Creature* creature)   :   ScriptedAI(creature) { }
+        npc_reward_shopAI(Creature *creature) : ScriptedAI(creature) {}
         uint32 say_timer;
         bool canSay;
 
@@ -203,7 +200,7 @@ public:
             canSay = false;
         }
 
-        void MoveInLineOfSight(Unit* who)
+        void MoveInLineOfSight(Unit *who)
         {
             if (me->IsWithinDist(who, 5.0f) && who->GetTypeId() == TYPEID_PLAYER)
             {
@@ -227,9 +224,8 @@ public:
             else
                 say_timer -= diff;
         }
-
     };
-    CreatureAI* GetAI(Creature* creature) const
+    CreatureAI *GetAI(Creature *creature) const
     {
         return new npc_reward_shopAI(creature);
     }
